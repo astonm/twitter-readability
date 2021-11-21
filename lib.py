13e27b69f8726f2b295fca50e224af5b2c631e8e
Aston Motes <astonm@gmail.com>
2021-11-21 13:05:52 -0800

    apply black formatting

--- lib.py
+++ lib.py
@@ -7,11 +7,12 @@ import readability
 
 clean = re.compile(r"(:?@|http)[^ ]+", re.IGNORECASE)
 
+
 def create_client():
-    consumer_key=""
-    consumer_secret=""
-    access_token=""
-    access_token_secret=""
+    consumer_key = ""
+    consumer_secret = ""
+    access_token = ""
+    access_token_secret = ""
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -19,20 +20,33 @@ def create_client():
 
     return api
 
-def get_reading_level(api, screen_name, count=200):
-    public_tweets = api.user_timeline(tweet_mode="extended", count=count, screen_name=screen_name)
-    all_text = "\n".join([clean.sub("", x.full_text) for x in public_tweets if x.retweeted==False \
-        and (x.in_reply_to_screen_name is None or x.in_reply_to_screen_name == x.user.screen_name)])
 
-    results = readability.getmeasures(all_text, lang='en')['readability grades']
+def get_reading_level(api, screen_name, count=200):
+    public_tweets = api.user_timeline(
+        tweet_mode="extended", count=count, screen_name=screen_name
+    )
+    all_text = "\n".join(
+        [
+            clean.sub("", x.full_text)
+            for x in public_tweets
+            if x.retweeted == False
+            and (
+                x.in_reply_to_screen_name is None
+                or x.in_reply_to_screen_name == x.user.screen_name
+            )
+        ]
+    )
+
+    results = readability.getmeasures(all_text, lang="en")["readability grades"]
 
     return results
+
 
 if __name__ == "__main__":
     client = create_client()
     results = get_reading_level(client, sys.argv[1])
 
-    os.system('cls')
+    os.system("cls")
 
     for item in results:
-        print(f'{item} : {results[item]}')
+        print(f"{item} : {results[item]}")
